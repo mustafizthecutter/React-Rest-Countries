b603cb320a92e5cd9b2f9ca60d590a5895b4a551
Mustafizur Rahman <mustafizur232@gmail.com>
2023-11-03 11:18:10 +0600

    rest countries code and toggle the button done

--- src/components/Country/Country.jsx
+++ src/components/Country/Country.jsx
@@ -1,11 +1,23 @@
+import { useState } from 'react';
 import './Country.css'
 const Country = ({ country }) => {
-    const { name, flags } = country;
-    console.log(country);
+    const { name, flags, population, area, cca3 } = country;
+    // console.log(country);
+    const [visited, setVisited] = useState(false);
+    const handleVisited = () => {
+        setVisited(!visited);
+    }
+
     return (
         <div className='box'>
             <h3>Name : {name?.common}</h3>
             <img src={flags?.png} alt="" />
+            <p>Population: {population}</p>
+            <p>Area: {area}</p>
+            <p><small>Code: {cca3}</small></p>
+            <button onClick={handleVisited}>{visited ? 'visited' : 'Going'}</button>
+            {visited ? 'I have visited the country' : 'I want to visit'}
+
         </div>
     );
 };
