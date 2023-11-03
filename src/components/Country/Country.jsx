a48f84832d9519263b23050ed98411f24210d2d9
Mustafizur Rahman <mustafizur232@gmail.com>
2023-11-03 14:03:34 +0600

    mark visited data added to a list by clicking a specific button done

--- src/components/Country/Country.jsx
+++ src/components/Country/Country.jsx
@@ -1,6 +1,6 @@
 import { useState } from 'react';
 import './Country.css'
-const Country = ({ country }) => {
+const Country = ({ country, handleVisitedCountry }) => {
     const { name, flags, population, area, cca3 } = country;
     // console.log(country);
     const [visited, setVisited] = useState(false);
@@ -9,12 +9,14 @@ const Country = ({ country }) => {
     }
 
     return (
-        <div className='box'>
-            <h3>Name : {name?.common}</h3>
+        <div className={`country ${visited ? 'visited' : 'non-visited'}`}>
+            <h3 style={{ color: visited ? 'red' : 'blue' }}> {name?.common}</h3>
             <img src={flags?.png} alt="" />
             <p>Population: {population}</p>
             <p>Area: {area}</p>
             <p><small>Code: {cca3}</small></p>
+            <button onClick={() => handleVisitedCountry(country)}>Mark Visited</button>
+            <br />
             <button onClick={handleVisited}>{visited ? 'visited' : 'Going'}</button>
             {visited ? 'I have visited the country' : 'I want to visit'}
 
